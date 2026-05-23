7780bf9f116f40f561f44ee81aaa846b4060284b
0n-uk <Connorfareth@gmail.com>
2026-05-23 11:57:20 -0400

    edited info button position to be lower, and added card background image

--- CardRenderer.java
+++ CardRenderer.java
@@ -1,8 +1,8 @@
-import javax.swing.*;
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class CardRenderer {
 
@@ -20,7 +20,7 @@ public class CardRenderer {
     static final int INFO_W   = 36;
     static final int INFO_H   = 18;
     static final int INFO_X   = (CARD_W - INFO_W) / 2;
-    static final int INFO_Y   = CARD_H - BOX - INFO_H - 2;
+    static final int INFO_Y   = CARD_H - INFO_H - 2;
 
     private static Font handFont;
     private static BufferedImage cardBg;
